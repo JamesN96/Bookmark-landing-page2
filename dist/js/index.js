1eb6511b2e39a446cb4a9a9827e19630a7811623
James Nguyen <james.nguyenn96@gmail.com>
2019-11-13 23:04:40 +1100

    Edit displayTab function to add active class and add new DOM selectors

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,6 +9,9 @@ const tab3 = document.getElementById("tab3");
 const div1 = document.getElementById("tab-1");
 const div2 = document.getElementById("tab-2");
 const div3 = document.getElementById("tab-3");
+const a1 = document.getElementById("a1");
+const a2 = document.getElementById("a2");
+const a3 = document.getElementById("a3");
 
 // Event Listener
 
@@ -35,13 +38,22 @@ function displayTab(e) {
     div1.style.visibility = "visible";
     div2.style.display = "none";
     div3.style.display = "none";
+    a1.classList.add("active");
+    a2.className = "";
+    a3.className = "";
   } else if (e.target === tab2) {
     div1.style.visibility = "hidden";
     div3.style.display = "none";
     div2.style.display = "block";
+    a2.classList.add("active");
+    a1.className = "";
+    a3.className = "";
   } else if (e.target === tab3) {
     div1.style.visibility = "hidden";
     div2.style.display = "none";
     div3.style.display = "block";
+    a3.classList.add("active");
+    a2.className = "";
+    a1.className = "";
   }
 }
