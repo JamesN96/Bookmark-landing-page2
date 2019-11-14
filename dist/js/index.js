0c3b5054c53843a7fa1020aa5b2c417a0d8da741
James Nguyen <james.nguyenn96@gmail.com>
2019-11-14 20:24:55 +1100

    Recreate displayTab function and active funciton. Add new dom selectors and add eventlisteners to them

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,24 +3,22 @@
 const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
-const tab1 = document.getElementById("tab1");
-const tab2 = document.getElementById("tab2");
-const tab3 = document.getElementById("tab3");
-const div1 = document.getElementById("tab-1");
-const div2 = document.getElementById("tab-2");
-const div3 = document.getElementById("tab-3");
 const a1 = document.getElementById("a1");
 const a2 = document.getElementById("a2");
 const a3 = document.getElementById("a3");
+const img = document.querySelector(".tab1-img");
+const h2 = document.querySelector(".tab1-h2");
+const p = document.querySelector(".tab1-p");
+
 const arrows = document.querySelectorAll(".arrow");
 
 // Event Listener
 
 menuBtn.addEventListener("click", openMenu);
 closeBtn.addEventListener("click", openMenu);
-tab1.addEventListener("click", displayTab);
-tab2.addEventListener("click", displayTab);
-tab3.addEventListener("click", displayTab);
+a1.addEventListener("click", displayTab);
+a2.addEventListener("click", displayTab);
+a3.addEventListener("click", displayTab);
 arrows.forEach(function(arrow) {
   arrow.addEventListener("click", showDiv);
 });
@@ -35,30 +33,43 @@ function openMenu(e) {
   }
 }
 
-// Display function
-
 function displayTab(e) {
-  if (e.target === tab1) {
-    div1.style.visibility = "visible";
-    div2.style.display = "none";
-    div3.style.display = "none";
-    a1.classList.add("active");
-    a2.className = "";
-    a3.className = "";
-  } else if (e.target === tab2) {
-    div1.style.visibility = "hidden";
-    div3.style.display = "none";
-    div2.style.display = "block";
-    a2.classList.add("active");
-    a1.className = "";
-    a3.className = "";
-  } else if (e.target === tab3) {
-    div1.style.visibility = "hidden";
-    div2.style.display = "none";
-    div3.style.display = "block";
-    a3.classList.add("active");
-    a2.className = "";
-    a1.className = "";
+  if (e.target === a1) {
+    img.src = " dist/img/illustration-features-tab-1.svg";
+    h2.textContent = "Bookmark in one click";
+    p.textContent =
+      " Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.";
+    active(e);
+  } else if (e.target === a2) {
+    img.src = "dist/img/illustration-features-tab-2.svg";
+    h2.textContent = "Intelligent Search";
+    p.textContent =
+      " Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.";
+    active(e);
+  } else {
+    img.src = "dist/img/illustration-features-tab-3.svg";
+    h2.textContent = "Share your bookmarks";
+    p.textContent =
+      "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.";
+    active(e);
+  }
+}
+
+// active class function
+
+function active(e) {
+  if (e.target === a1) {
+    a1.parentElement.classList.add("active");
+    a2.parentElement.className = "";
+    a3.parentElement.className = "";
+  } else if (e.target === a2) {
+    a2.parentElement.classList.add("active");
+    a1.parentElement.className = "";
+    a3.parentElement.className = "";
+  } else {
+    a3.parentElement.classList.add("active");
+    a1.parentElement.className = "";
+    a2.parentElement.className = "";
   }
 }
 
